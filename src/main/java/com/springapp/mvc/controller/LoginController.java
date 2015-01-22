d0e15443e5f862bddef63ae1c509f2d6e79b6ae3
acer <punrewatg7@gmail.com>
2015-01-22 13:31:10 +0545

    first assignment completed logincontroller added

---
+++ src/main/java/com/springapp/mvc/controller/LoginController.java
@@ -0,0 +1,39 @@
+package com.springapp.mvc.controller;
+
+import com.springapp.mvc.database.Database;
+import com.springapp.mvc.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.sql.SQLException;
+
+/**
+ * Created by acer on 1/21/2015.
+ */
+@Controller
+public class LoginController {
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView user() {
+        return new ModelAndView("login", "command", new User());
+    }
+
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String welcome(@ModelAttribute("username") User user, ModelMap model) throws SQLException, ClassNotFoundException {
+
+        Database d = new Database();
+        boolean result = d.DbConnection(user.getUsername(), user.getPassword());
+        if (result == true) {
+            return  "welcome";
+        } else {
+            return  "error";
+
+        }
+
+    }
+}
+
